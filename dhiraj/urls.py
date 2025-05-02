b15e4dc758ea6ccade1c3f374c023467bc17e800
Dhiraj kumar <149323248+Dhirajsingh12@users.noreply.github.com>
2025-05-02 21:48:22 +0530

    django

--- dhiraj/urls.py
+++ dhiraj/urls.py
@@ -21,5 +21,6 @@ from dhiraj import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hp/',views.hp),
     path('aboutme/',views.aboutme),
 ]
--- dhiraj/views.py
+++ dhiraj/views.py
@@ -1,3 +1,7 @@
+from django.shortcuts import render
+def hp(request):
+    data={'title':'newtitle django','number':[12,45,3,5,24,24]}
+    return render(request,'index.html',data)
 from django.http import HttpResponse
 def aboutme(request):
     return HttpResponse("welcome to django view")
